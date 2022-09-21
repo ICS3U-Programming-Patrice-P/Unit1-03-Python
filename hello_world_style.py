2b3dc3a5071d3a7300d29307b0fc2b24ba07c3ef
Patrice Pat-Odita <patrice.pat-odita@stu.ocsb.ca>
2022-09-21 05:00:08 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created By: Patrice Pat-Odita
+# Date: sep. 20, 2022
+# This program displays Hello, World! with proper style.
+
+
+def main():
+    print("Hello, World! with proper style!")
+
+
+if __name__ == "__main__":
+    main()
